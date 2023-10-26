19ebba9e84943eca0f78882731c9af0f74c0f10a
Ryan Clements <ryanclementshax@gmail.com>
2023-10-26 10:01:47 -0400

    ci(playwright): write media query tests

---
+++ e2e/tests/mediaQuery.spec.ts
@@ -0,0 +1,60 @@
+import { expect } from '@playwright/test'
+import { test } from '../test_repos/test'
+
+test('can enable a theme using a media query', async ({ page, testRepo }) => {
+  await testRepo.openWithConfig({
+    defaultTheme: {
+      extend: {
+        colors: {
+          primary: 'blue'
+        }
+      }
+    },
+    themes: [
+      {
+        name: 'darkTheme',
+        mediaQuery: '@media (prefers-color-scheme: dark)',
+        extend: {
+          colors: {
+            primary: 'red'
+          }
+        }
+      }
+    ]
+  })
+
+  await page.emulateMedia({ colorScheme: 'dark' })
+
+  await expect(page).toHaveScreenshot()
+})
+
+// TODO fix this test's snapshots
+test('cant enable a theme using the theme name as a class if a media query provided', async ({
+  page,
+  testRepo
+}) => {
+  await testRepo.openWithConfig({
+    defaultTheme: {
+      extend: {
+        colors: {
+          primary: 'blue'
+        }
+      }
+    },
+    themes: [
+      {
+        name: 'darkTheme',
+        mediaQuery: '@media (prefers-color-scheme: dark)',
+        extend: {
+          colors: {
+            primary: 'red'
+          }
+        }
+      }
+    ]
+  })
+
+  await testRepo.setClassOnRoot('darkTheme')
+
+  await expect(page).toHaveScreenshot()
+})
