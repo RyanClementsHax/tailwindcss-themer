2a0f16366b524c73ad1672b71184e6dd885f2d3e
Ryan Clements <ryanclementshax@gmail.com>
2023-11-03 10:06:08 -0400

    ci(playwright): update background to be darker to avoid eye strain

--- e2e/test_repos/templates/create-react-app/src/App.tsx
+++ e2e/test_repos/templates/create-react-app/src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeNode } from './components/ThemeNode'
 function App() {
   const [numThemeNodes, setNumThemeNodes] = useState(0)
   return (
-    <div className="mx-auto flex flex-col px-5 py-16 font-mono text-gray-900">
+    <div className="mx-auto flex flex-col px-5 py-16 font-mono text-slate-900">
       <header>
         <h1 className="mb-5 text-center text-4xl font-black">
           Mainitainers &lt;3 integration tests
@@ -16,7 +16,7 @@ function App() {
           return <ThemeNode key={nodeId} nodeId={nodeId} />
         })}
         <button
-          className="rounded-md border-0 bg-white px-4 py-3 shadow-md ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:outline-none"
+          className="rounded-md border-0 bg-white px-4 py-3 shadow-md ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:outline-none"
           onClick={() => setNumThemeNodes(num => num + 1)}
         >
           Add theme node
--- e2e/test_repos/templates/create-react-app/src/components/ThemeNode.tsx
+++ e2e/test_repos/templates/create-react-app/src/components/ThemeNode.tsx
@@ -10,7 +10,7 @@ export function ThemeNode({ nodeId }: ThemeNodeProps) {
   return (
     <section
       data-testid={`theme-node-${nodeId}`}
-      className="flex w-full flex-col gap-5 rounded-md border border-gray-200 bg-white px-8 py-5 shadow-md"
+      className="flex w-full flex-col gap-5 rounded-md border border-slate-200 bg-white px-8 py-5 shadow-md"
     >
       <h2 className="text-2xl font-bold">Theme node {nodeId}</h2>
       <AttributesInput
@@ -72,11 +72,11 @@ function AttributesInput({
         <input
           id={inputId}
           spellCheck="false"
-          className="w-32 rounded-md border-0 px-4 py-3 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:outline-none"
+          className="w-32 rounded-md border-0 px-4 py-3 shadow-sm ring-1 ring-inset ring-slate-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:outline-none"
           value={attributesInput}
           onChange={e => setAttributesInput(e.target.value)}
         />
-        <pre className="w-full rounded-md bg-gray-100 px-4 py-3">
+        <pre className="w-full rounded-md bg-slate-100 px-4 py-3">
           <code>{JSON.stringify(parsedAttributes, null, 2)}</code>
         </pre>
       </div>
@@ -92,7 +92,7 @@ function ThemedContent({ attributes }: ThemedContentProps) {
   return (
     <div className="flex flex-col gap-2 text-sm">
       Themed content
-      <div className="rounded-md border border-gray-200 p-5">
+      <div className="rounded-md border border-slate-200 p-5">
         <div {...attributes}>
           <div className="bg-primary h-10 w-10 rounded-md"></div>
         </div>
