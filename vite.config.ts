39c72fd131d3316ccef3bfff5208cacc3c62cba1
Ryan Clements <ryanclementshax@gmail.com>
2024-10-26 14:55:45 -0400

    ci(cov): report coverage on PR

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,20 @@ import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'e2e', 'examples'],
+    dir: 'src',
     coverage: {
+      enabled: true,
       include: ['src'],
-      enabled: true
+      // defaults https://vitest.dev/guide/coverage.html#coverage-setup
+      // the json-summary and json reporters are for reporting in PR
+      // https://github.com/marketplace/actions/vitest-coverage-report#usage
+      reporter: [
+        ...(configDefaults.coverage.reporter ?? []),
+        'json-summary',
+        'json'
+      ],
+      // So we get coverage reports even if tests fail
+      reportOnFailure: true
     }
   }
 })
